62b5057bd16059c902f439514a13306becb30604
wanglin <adamslin@foxmail.com>
2025-02-09 20:49:21 +0800

    fix: e2e test error

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -199,7 +199,7 @@ var _ = Describe("Manager", Ordered, func() {
 				cmd := exec.Command("kubectl", "get", "endpoints", metricsServiceName, "-n", namespace)
 				output, err := utils.Run(cmd)
 				g.Expect(err).NotTo(HaveOccurred())
-				g.Expect(output).To(ContainSubstring("8443"), "Metrics endpoint is not ready")
+				g.Expect(output).To(ContainSubstring("8080"), "Metrics endpoint is not ready")
 			}
 			Eventually(verifyMetricsEndpointReady).Should(Succeed())
 
